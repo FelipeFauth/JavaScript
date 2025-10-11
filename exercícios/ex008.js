95a36c21112c2b5e2ebe66f3fb3fd97af4d42cb3
Felipe <felipefauthschecheleski@gmail.com>
2025-10-11 18:56:21 -0300

    ex008 e teste005 concluídos

---
+++ exercícios/ex008.js
@@ -0,0 +1,8 @@
+let vel = 79
+if (vel > 80) {
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    console.log('Você ultrapassou o limite de velocidade. Multado!')
+}else {
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    console.log('Diriga com segurança')
+}
